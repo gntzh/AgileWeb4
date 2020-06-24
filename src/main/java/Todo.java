ba3aa1eec5077ff07352f7d557847afe0aac9ebf
shoor <shoorday@gmail.com>
2020-06-24 18:15:24 +0800

    Todo Bean

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,9 +1,71 @@
 public class Todo {
     private String name;
+    private String detail;
     private Boolean finished;
     private String id;
 
-    public void Todo(String name, Boolean finished){
+    public Todo(String name, Boolean finished) {
+        this.name = name;
+        this.finished = finished;
+    }
 
+    public Todo(String name, String detail, Boolean finished) {
+        this.name = name;
+        this.detail = detail;
+        this.finished = finished;
+    }
+
+    public Todo(String name, Boolean finished, String id) {
+        this.name = name;
+        this.finished = finished;
+        this.id = id;
+    }
+
+    public Todo(String name, Boolean finished, String detail, String id) {
+        this.name = name;
+        this.detail = detail;
+        this.finished = finished;
+        this.id = id;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getFinished() {
+        return finished;
+    }
+
+    public void setFinished(Boolean finished) {
+        this.finished = finished;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                ", id='" + id + '\'' +
+                "name='" + name + '\'' +
+                ", finished=" + finished +
+                '}';
     }
 }
